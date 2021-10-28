7525160ee089ac6ac55474c3a4746ca2a0231ae0
aleksandrakovacevic <aleksandra.kovacevic@tnation.eu>
2021-10-28 11:02:33 +0200

    Finish Catalog microservice

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    //any class that inherits this interface should provide products collection
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
